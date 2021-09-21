f7495ffd160364dcc6d0570ef09a6a591cbaab61
Roman Pinyazhin <pinyazhin@haulmont.com>
2021-09-21 15:36:59 +0400

    Provide ability to use CUBA filter implementation in screens with Jmix XSD (81)

--- cuba/src/main/java/com/haulmont/cuba/gui/xml/layout/CubaLoaderConfig.java
+++ cuba/src/main/java/com/haulmont/cuba/gui/xml/layout/CubaLoaderConfig.java
@@ -124,6 +124,7 @@ public class CubaLoaderConfig extends BaseLoaderConfig implements LoaderConfig {
 
         loaders.put("frame", CubaFragmentComponentLoader.class);
         loaders.put("fragment", CubaFragmentComponentLoader.class);
+        loaders.put("cubaFilter", CubaFilterLoader.class);
 
         loaders.put(Calendar.NAME, CubaCalendarLoader.class);
         loaders.put(Tree.NAME, CubaTreeLoader.class);
